a2ee86554808de385752c1bbab9879c60a4e2471
SAI KARTHIK <SAI KARTHIK@KSaiKarthik.mshome.net>
2020-09-24 16:26:46 +0530

    Spring Backend- Commit :01

---
+++ Web_Proj-1/src/main/java/com/dxc/WebProj1Application.java
@@ -0,0 +1,13 @@
+package com.dxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProj1Application.class, args);
+	}
+
+}
